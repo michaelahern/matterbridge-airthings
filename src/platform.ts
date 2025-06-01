8ec18acfdd931ad4fd35fd35674ecdfa4976b80b
Michael Ahern <michael.ahern@gmail.com>
2025-06-01 16:43:43 -0400

    Initial Commit (#2)

--- src/platform.ts
+++ src/platform.ts
@@ -1,7 +1,7 @@
-import { AirthingsClient, SensorUnits } from 'airthings-consumer-api';
-import { Matterbridge, MatterbridgeEndpoint, MatterbridgeDynamicPlatform, PlatformConfig, bridgedNode, humiditySensor, powerSource, temperatureSensor } from 'matterbridge';
+import { AirthingsClient, SensorResult, SensorUnits } from 'airthings-consumer-api';
+import { Matterbridge, MatterbridgeEndpoint, MatterbridgeDynamicPlatform, PlatformConfig, airQualitySensor, bridgedNode, humiditySensor, powerSource, temperatureSensor } from 'matterbridge';
 import { AnsiLogger } from 'matterbridge/logger';
-import { PowerSource, RelativeHumidityMeasurement, TemperatureMeasurement } from 'matterbridge/matter/clusters';
+import { AirQuality, PowerSource, RelativeHumidityMeasurement, TemperatureMeasurement } from 'matterbridge/matter/clusters';
 
 export class AirthingsPlatform extends MatterbridgeDynamicPlatform {
     airthingsClient: AirthingsClient;
@@ -12,13 +12,16 @@ export class AirthingsPlatform extends MatterbridgeDynamicPlatform {
         super(matterbridge, log, config);
         this.log.info('[init]');
 
-        const clientId = process.env.AIRTHINGS_CLIENT_ID;
-        const clientSecret = process.env.AIRTHINGS_CLIENT_SECRET;
+        const clientId = config.clientId as string ?? process.env.AIRTHINGS_CLIENT_ID;
+        const clientSecret = config.clientSecret as string ?? process.env.AIRTHINGS_CLIENT_SECRET;
 
         if (!clientId || !clientSecret) {
             this.log.error('Must set the AIRTHINGS_CLIENT_ID and AIRTHINGS_CLIENT_SECRET environment variables, exiting...');
             process.exit(1);
         }
+
+        config.clientId = clientId;
+        config.clientSecret = clientSecret;
 
         this.airthingsClient = new AirthingsClient({
             clientId: clientId,
@@ -42,10 +45,11 @@ export class AirthingsPlatform extends MatterbridgeDynamicPlatform {
                 continue;
             }
 
+            const battery = deviceSensors.batteryPercentage;
             const temp = deviceSensors.sensors.find(s => s.sensorType === 'temp')?.value;
             const humidity = deviceSensors.sensors.find(s => s.sensorType === 'humidity')?.value;
 
-            const endpoint = new MatterbridgeEndpoint([bridgedNode, powerSource, temperatureSensor, humiditySensor], { uniqueStorageKey: 'Airthings' }, this.config.debug as boolean)
+            const endpoint = new MatterbridgeEndpoint([bridgedNode, powerSource], { uniqueStorageKey: 'Airthings-' + device.serialNumber }, this.config.debug as boolean)
                 .createDefaultBridgedDeviceBasicInformationClusterServer(
                     device.name,
                     device.serialNumber,
@@ -53,13 +57,26 @@ export class AirthingsPlatform extends MatterbridgeDynamicPlatform {
                     'Airthings',
                     device.type.replace(/_/g, ' ').toLowerCase().replace(/\b\w/g, char => char.toUpperCase()),
                     parseInt(this.version.replace(/\D/g, '')),
-                    this.version === '' ? 'Unknown' : this.version,
+                    this.version,
                     parseInt(this.matterbridge.matterbridgeVersion.replace(/\D/g, '')),
                     this.matterbridge.matterbridgeVersion
                 )
-                .createDefaultPowerSourceReplaceableBatteryClusterServer(deviceSensors.batteryPercentage, PowerSource.BatChargeLevel.Ok, 9000, 'AA', 6)
-                .createDefaultTemperatureMeasurementClusterServer(temp ? temp * 100 : null)
-                .createDefaultRelativeHumidityMeasurementClusterServer(humidity ? humidity * 100 : null);
+                .createDefaultPowerSourceReplaceableBatteryClusterServer(battery ? battery * 2 : undefined)
+                .addRequiredClusterServers();
+
+            endpoint.addChildDeviceType('Temperature', temperatureSensor)
+                .createDefaultTemperatureMeasurementClusterServer(temp ? temp * 100 : undefined)
+                .addRequiredClusterServers();
+
+            endpoint.addChildDeviceType('Humidity', humiditySensor)
+                .createDefaultRelativeHumidityMeasurementClusterServer(humidity ? humidity * 100 : undefined)
+                .addRequiredClusterServers();
+
+            endpoint.addChildDeviceType('AirQuality', airQualitySensor)
+                .createDefaultAirQualityClusterServer(this.#getAirQuality(deviceSensors))
+                .createDefaultTemperatureMeasurementClusterServer(temp ? temp * 100 : undefined)
+                .createDefaultRelativeHumidityMeasurementClusterServer(humidity ? humidity * 100 : undefined)
+                .addRequiredClusterServers();
 
             this.setSelectDevice(device.serialNumber, device.name, undefined, 'hub');
             await this.registerDevice(endpoint);
@@ -79,25 +96,36 @@ export class AirthingsPlatform extends MatterbridgeDynamicPlatform {
                     this.log.debug(`Refreshing sensors for ${device.serialNumber}:`, device);
 
                     const batteryPercentage = device.batteryPercentage;
-                    if (batteryPercentage) {
-                        await endpoint.setAttribute(PowerSource.Cluster.id, 'batPercentRemaining', batteryPercentage, endpoint.log);
+                    if (batteryPercentage !== undefined) {
+                        await endpoint.setAttribute(PowerSource.Cluster.id, 'batPercentRemaining', batteryPercentage * 2, endpoint.log);
                     }
 
                     const temp = device.sensors.find(s => s.sensorType === 'temp')?.value;
-                    if (temp) {
-                        await endpoint.setAttribute(TemperatureMeasurement.Cluster.id, 'measuredValue', temp * 100, endpoint.log);
+                    if (temp !== undefined) {
+                        const tempEndpoint = endpoint.getChildEndpointByName('Temperature');
+                        await tempEndpoint?.setAttribute(TemperatureMeasurement.Cluster.id, 'measuredValue', temp * 100, endpoint.log);
                     }
 
                     const humidity = device.sensors.find(s => s.sensorType === 'humidity')?.value;
-                    if (humidity) {
-                        await endpoint.setAttribute(RelativeHumidityMeasurement.Cluster.id, 'measuredValue', humidity * 100, endpoint.log);
+                    if (humidity !== undefined) {
+                        const humidityEndpoint = endpoint.getChildEndpointByName('Humidity');
+                        await humidityEndpoint?.setAttribute(RelativeHumidityMeasurement.Cluster.id, 'measuredValue', humidity * 100, endpoint.log);
+                    }
+
+                    const airQualityEndpoint = endpoint.getChildEndpointByName('AirQuality');
+                    await airQualityEndpoint?.setAttribute(AirQuality.Cluster.id, 'airQuality', this.#getAirQuality(device), endpoint.log);
+                    if (temp !== undefined) {
+                        await airQualityEndpoint?.setAttribute(TemperatureMeasurement.Cluster.id, 'measuredValue', temp * 100, endpoint.log);
+                    }
+                    if (humidity !== undefined) {
+                        await airQualityEndpoint?.setAttribute(RelativeHumidityMeasurement.Cluster.id, 'measuredValue', humidity * 100, endpoint.log);
                     }
                 }
             }
         };
 
         refreshSensors();
-        this.refreshSensorsInterval = setInterval(refreshSensors, 120 * 1000);
+        this.refreshSensorsInterval = setInterval(refreshSensors, (this.config.refreshInterval as number ?? 120) * 1000);
     }
 
     override async onShutdown(reason?: string) {
@@ -109,5 +137,76 @@ export class AirthingsPlatform extends MatterbridgeDynamicPlatform {
         if (this.config.unregisterOnShutdown === true) {
             await this.unregisterAllDevices(500);
         }
+    }
+
+    #getAirQuality(lastResult: SensorResult) {
+        let aq = AirQuality.AirQualityEnum.Unknown;
+
+        const co2Sensor = lastResult.sensors.find(x => x.sensorType === 'co2');
+        if (co2Sensor /* && !this.airthingsConfig.co2AirQualityDisabled */) {
+            if (co2Sensor.value >= 1000) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Poor);
+            }
+            else if (co2Sensor.value >= 800) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Fair);
+            }
+            else {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Good);
+            }
+        }
+
+        const humiditySensor = lastResult.sensors.find(x => x.sensorType === 'humidity');
+        if (humiditySensor /* && !this.airthingsConfig.humidityAirQualityDisabled */) {
+            if (humiditySensor.value < 25 || humiditySensor.value >= 70) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Poor);
+            }
+            else if (humiditySensor.value < 30 || humiditySensor.value >= 60) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Fair);
+            }
+            else {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Good);
+            }
+        }
+
+        const pm25Sensor = lastResult.sensors.find(x => x.sensorType === 'pm25');
+        if (pm25Sensor /* && !this.airthingsConfig.pm25AirQualityDisabled */) {
+            if (pm25Sensor.value >= 25) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Poor);
+            }
+            else if (pm25Sensor.value >= 10) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Fair);
+            }
+            else {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Good);
+            }
+        }
+
+        const radonShortTermAvgSensor = lastResult.sensors.find(x => x.sensorType === 'radonShortTermAvg');
+        if (radonShortTermAvgSensor /* && !this.airthingsConfig.radonAirQualityDisabled */) {
+            if (radonShortTermAvgSensor.value >= 150) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Poor);
+            }
+            else if (radonShortTermAvgSensor.value >= 100) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Fair);
+            }
+            else {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Good);
+            }
+        }
+
+        const vocSensor = lastResult.sensors.find(x => x.sensorType === 'voc');
+        if (vocSensor /* && !this.airthingsConfig.vocAirQualityDisabled */) {
+            if (vocSensor.value >= 2000) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Poor);
+            }
+            else if (vocSensor.value >= 250) {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Fair);
+            }
+            else {
+                aq = Math.max(aq, AirQuality.AirQualityEnum.Good);
+            }
+        }
+
+        return aq;
     }
 }
